e0c33593f2e164caa7fc019eef9217416e53dc70
Stephen Ellema <stephensean.ellema.cics@ust.edu.ph>
2022-03-19 16:57:16 +0800

    page 10

---
+++ page10.js
@@ -0,0 +1,7 @@
+let base = document.getElementById('base');
+let height = document.getElementById('height');
+
+function myFunction(base, height) {
+  return base * height;
+  alert(myFunction());
+}
